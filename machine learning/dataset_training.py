3c698edc9e9f353da854cc00ca5cd6f2af0fbf0e
Sophie Wollitzer <s.g.wollitzer@wustl.edu>
2025-11-23 19:44:59 -0600

    outline for ml file

---
+++ machine learning/dataset_training.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+
+def loadEmailDataset(filepath):
+    pass
+
+def preprocess(df):
+    pass
+
+def extractSenderFeatures(df):
+    pass
+
+def extractSubjectFeatures(df):
+    pass
+
+def exttractBodyFeatures(df):
+    pass
+
+def extractURLFeatures(df):
+    pass
+
+def createFeatureMatrix(df):
+    pass
+
+def normalize(X_train,X_test):
+    pass
+
+def binaryClassificationError(w,X,y):
+    pass
+
+def LogisticRegL2(X,y,w_init,max_its,eta,reg,grad_threshold):
+    pass
+
+def splitDataset(df,test_ratio=0.2):
+    pass
+
+def run():
+    pass
+
+if __name__=="__main__":
+    run()
